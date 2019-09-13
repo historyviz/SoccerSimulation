556d12ec8c9019e44a10ebbbf76d1f61b12f593b
Dillon Cutaiar <dillonc@vt.edu>
2019-09-13 14:57:24 -0400

    Commiting Unity project along with some meaty ignore files. Note that vive json configuration files have been commited.

---
+++ Soccer Simulation/Assets/SoccerSimulation/Opponent/Dillons Opponenet/ExperienceController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExperienceController : MonoBehaviour
+{
+    [Header("Assign These!")]
+    [Tooltip("Assign me from the scene!")]
+    public SoccerPlayerController soccerPlayerController;
+
+    [Tooltip("Assign me from the scene!")]
+    public Transform slideTackleStartLocation;
+
+    [Tooltip("Assign me from the scene!")]
+    public Transform opponenetStartLocation;
+
+    [Tooltip("Assign me from the scene!")]
+    public AudioSource sceneAudioSource;
+
+    // TODO: Want to remove eventually
+    [Tooltip("Assign me from the scene!")]
+    public Button startButton;
+
+    [Header("Sound")]
+    public AudioClip startSound;
+    public AudioClip ambientTrack;
+    public bool mute = false;
+
+    private bool evaluateSoccerPlayerLocation = false;
+    private float slideTackleDistanceThreshoold = 0.5f;
+
+    void Start()
+    {
+        // Set up audio stuff
+        sceneAudioSource.loop = true;
+        sceneAudioSource.clip = ambientTrack;
+        if (!mute) sceneAudioSource.Play();
+
+        // Set up button events
+        // TODO: remove eventually
+        startButton.onClick.AddListener(() => { startTackleExperience(); startButton.interactable = false; });
+    }
+    void Update()
+    {
+        if (evaluateSoccerPlayerLocation) evaluateSoccerPlayerLocationLoop();
+    }
+
+    [ContextMenu("startTackleExperience")]
+    public void startTackleExperience()
+    {
+        soccerPlayerController.IsRunning = true;
+        soccerPlayerController.Target = slideTackleStartLocation.position;
+        evaluateSoccerPlayerLocation = true;
+        if (!mute) sceneAudioSource.PlayOneShot(startSound);
+    }
+
+    private void evaluateSoccerPlayerLocationLoop()
+    {
+        if (Vector3.Distance(soccerPlayerController.gameObject.transform.position, slideTackleStartLocation.position) < slideTackleDistanceThreshoold)
+        {
+            soccerPlayerController.StartTackle();
+            evaluateSoccerPlayerLocation = false;
+            // Wait unitl tackle is over, then return to start
+            // TODO: Come up with a better system than chain invokes lol
+            Invoke("returnToStart", 2);
+        }
+    }
+
+    // TODO: Come up with a better system than chain invokes lol (remove)
+    private void returnToStart()
+    {
+        soccerPlayerController.Target = opponenetStartLocation.position;
+        soccerPlayerController.IsRunning = true;
+        Invoke("facePlayer", 2);
+    }
+
+    // TODO: Come up with a better system than chain invokes lol (remove)
+    private void facePlayer()
+    {
+        soccerPlayerController.IsRunning = false;
+        soccerPlayerController.FaceDirection(opponenetStartLocation.forward);
+
+        // ugh, I have to put this here :(
+        // TODO: remove
+        startButton.interactable = true;
+    }
+}
---
+++ Soccer Simulation/Assets/SoccerSimulation/Opponent/Dillons Opponenet/SoccerPlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SoccerPlayerController : MonoBehaviour
+{
+    [Tooltip("Parameter that controls running animation. Should be a Boolean type.")]
+    public string runningParameter;
+
+    [Tooltip("Parameter that controls tackling animation. Should be a Trigger type.")]
+    public string tackleParameter;
+
+    // Reference to the animator and agent
+    private Animator animator;
+    private NavMeshAgent navMeshAgent;
+
+    // Abstract running animation state. Using IsRunning guaruntees animation is correct.
+    private bool isRunning = false;
+    public bool IsRunning
+    {
+        get { return isRunning; }
+        set
+        {
+            isRunning = value;
+            animator.SetBool(runningParameter, isRunning);
+        }
+    }
+
+    // Abstract agent destination so other classes only see SoccerPlayerComtroller.Target
+    public Vector3 Target
+    {
+        get
+        {
+            return navMeshAgent.destination;
+        }
+        set
+        {
+            navMeshAgent.destination = value;
+        }
+    }
+
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        navMeshAgent = GetComponentInChildren<NavMeshAgent>();
+
+    }
+
+    public void StartTackle()
+    {
+        animator.SetTrigger(tackleParameter);
+        IsRunning = false;
+    }
+
+    public void FaceDirection(Vector3 direction)
+    {
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
